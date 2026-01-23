4a9e3b2109d8751fb6756294681efbcbedecdcc1
Alco2 <loordabdou@gmail.com>
2026-01-23 18:46:30 +0100

    remove test code

--- backend/main.go
+++ backend/main.go
@@ -1,34 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"main/security"
+	"log"
+	"main/route"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	router := gin.Default()
 
-	const temp = "test"
-	encrypted := security.Encrypt(temp)
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
-	fmt.Println("encrypted: ", encrypted)
+	route.CreateRoutes(router)
 
-	decrypted := security.Decrypt(encrypted)
-
-	fmt.Println("decrypted: ", decrypted)
-	// router := gin.Default()
-
-	// router.Use(cors.New(cors.Config{
-	// 	AllowOrigins:     []string{"http://localhost:5173"},
-	// 	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-	// 	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-	// 	ExposeHeaders:    []string{"Content-Length"},
-	// 	AllowCredentials: true,
-	// 	MaxAge:           12 * time.Hour,
-	// }))
-
-	// route.CreateRoutes(router)
-
-	// if err := router.Run(); err != nil {
-	// 	log.Fatalf("failed to run server: %v", err)
-	// }
+	if err := router.Run(); err != nil {
+		log.Fatalf("failed to run server: %v", err)
+	}
 }
